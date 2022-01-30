9192318d8c55a0e3d40e17a01e76f9e2d6ccdede
Danny McKinnon <dannymckinnon.dm@gmail.com>
2022-01-29 16:01:10 -0800

    Moved script tag to head and added defer attribute

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     return `It's a tie, You both chose ${computerSelection}.`;
   } else if (playerSelection === 'rock' && computerSelection === 'scissors' ||
-            playerSelection === 'paper' && computerSelection === 'rock' ||
-            playerSelection === 'scissors' && computerSelection === 'paper') {
+      playerSelection === 'paper' && computerSelection === 'rock' ||
+      playerSelection === 'scissors' && computerSelection === 'paper') {
     return `You win, ${playerSelection} beats ${computerSelection}.`;         
   } else {
     return `You lose, ${computerSelection} beats ${playerSelection}.`;
